27c9a1d73f5cf35e8cb58ab16ef64682dc502ee8
Maxime Lalo <maxime.lalo.pro@gmail.com>
2025-06-09 16:41:50 +0200

    :sparkles: Update RiddleAdmin to use IsContractActive for riddle submission control

--- src/pages/RiddleAdmin.tsx
+++ src/pages/RiddleAdmin.tsx
@@ -1,10 +1,18 @@
-import IsBot from "@/components/wrappers/IsBot";
 import SetRiddleForm from "@/components/materials/SetRiddleForm";
+import IsContractActive from "@/components/wrappers/IsContractActive";
 
 export default function RiddleAdmin() {
     return (
-        <IsBot>
-            <SetRiddleForm />
-        </IsBot>
+        <>
+            <IsContractActive not>
+                <SetRiddleForm />
+            </IsContractActive>
+            <IsContractActive>
+                <p>
+                    ⛔ The contract is active. You cannot set a new riddle while
+                    the contract is active.
+                </p>
+            </IsContractActive>
+        </>
     );
 }
